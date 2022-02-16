47b1f4a71d695174f0a22673d95812d33a36570f
203 <203@academicos.cesde.edu.co>
2022-02-16 10:06:23 -0500

    Se crea controlador

---
+++ js/controladorTienda.js
@@ -0,0 +1,16 @@
+let nombre_producto = 'Batimóvil'
+let precio_producto = 40000
+let peso_producto = 18.5
+let tiene_descuento = true
+let descripcion_producto = 'Batimóvil a escala'
+let telefono_vendedor = "3225962363"
+
+const NOMBRE_CLIENTE="Juan José"
+
+//SALIDAS
+
+console.log(nombre_producto)
+console.log("Su nombre es: " + NOMBRE_CLIENTE)
+console.log("El precio de su producto es: " + precio_producto)
+console.log(nombre_producto)
+console.log(nombre_producto)
