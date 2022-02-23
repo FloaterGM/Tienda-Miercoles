4123cfd70227b892768f081a58f676413d2de047
203 <203@academicos.cesde.edu.co>
2022-02-23 10:12:05 -0500

    llenado_de_arrays

--- js/controladorTienda.js
+++ js/controladorTienda.js
@@ -1,6 +1,6 @@
 //Controlando etiquetas
 
-let etq_titulo = document.getElementById('millos')
+/*let etq_titulo = document.getElementById('millos')
 
 etq_titulo.textContent = "KNY (Kimetsu No Yaiba)"
 
@@ -11,4 +11,20 @@ let etq_foto = document.getElementById('foto')
 etq_foto.src = "/img/descarga.jpg" 
 
 etq_titulo.classList.add('text-center')
-etq_titulo.classList.add('text-light')
+etq_titulo.classList.add('text-light')*/
+
+//Tienda 
+
+let usuario = {
+    
+    nombre: 'Juan Sebastian Diaz',
+    edad: 48,
+    amigos: ["Victor", "Cristian"]
+
+
+}
+
+let etq_titulo = document.getElementById('millos')
+
+etq_titulo.textContent = usuario.amigos[0];
+
---
+++ js/llenadoTienda.js
@@ -0,0 +1,7 @@
+let productos = [
+    {nombre: 'Batimovil', precio: 350000, descripcion: 'Batimovil a escala'},
+    {nombre: 'Camiseta', precio: 45000, descripcion: 'Camiseta original DC'}
+
+]
+
+console.log(productos)
