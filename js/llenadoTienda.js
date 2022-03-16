d24a6b4d58b09afa17857688ce84c9b4cdb24dd9
FloaterGM <99337469+FloaterGM@users.noreply.github.com>
2022-03-16 10:19:22 -0500

    Entrega

--- js/llenadoTienda.js
+++ js/llenadoTienda.js
@@ -1,15 +1,15 @@
 let productos = [
      
-        { img: "/img/inosuke.jpg", nombre: "Llaveros KNY", precio: 15000 },
-        { img: "/img/inosuke.jpg", nombre: "Llaveros KNY Katana", precio: 20000 },
-        { img: "/img/inosuke.jpg", nombre: "Camiseta Estampada", precio: 45000 },
-        { img: "/img/inosuke.jpg", nombre: "Buzo Estampado", precio: 70000 },
-        { img: "/img/inosuke.jpg", nombre: "Figuritas Representativas", precio: 60000 },
-        { img: "/img/inosuke.jpg", nombre: "Aretes Representativos (Tanjiro)", precio: 12000 },
-        { img: "/img/inosuke.jpg", nombre: "Posters KNY", precio: 10000 },
-        { img: "/img/inosuke.jpg", nombre: "Album KNY", precio: 25000 },
-        { img: "/img/inosuke.jpg", nombre: "Stickers KNY", precio: 5000 },
-        { img: "/img/inosuke.jpg", nombre: "Manillas KNY", precio: 10000 }
+        { img: "img/llaveros.jpg", nombre: "Llaveros KNY", desc: "Llaverito personalizado de KNY", precio: 15000 },
+        { img: "img/katana.jpg", nombre: "Katana KNY",  desc: "Katana personalizada de tamaño pequeño", precio: 25000 },
+        { img: "img/camiseta.jpg", nombre: "Camiseta Estampada",  desc: "Camisetas estamapadas con diseños de KNY", precio: 45000 },
+        { img: "img/buzo.jpg", nombre: "Buzo Estampado",  desc: "Buzos estamapados con diseños de KNY", precio: 70000 },
+        { img: "img/figuritas.jpg", nombre: "Figuritas Representativas",  desc: "Figuritas de los personajes del anime", precio: 60000 },
+        { img: "img/aretes.jpg", nombre: "Aretes Representativos (Tanjiro)",  desc: "Aretes de Kamado Tanjiro", precio: 12000 },
+        { img: "img/posters.jpg", nombre: "Posters KNY",  desc: "Posters representativos del anime", precio: 10000 },
+        { img: "img/album.jpg", nombre: "Album KNY",  desc: "Album de fotos con portada de KNY", precio: 25000 },
+        { img: "img/estampitas.jpg", nombre: "Stickers KNY",  desc: "Estampitas del anime", precio: 5000 },
+        { img: "img/collar.jpg", nombre: "Collar KNY",  desc: "Collar representativo", precio: 15000 }
 ]
 
 let fila = document.getElementById("fila");
@@ -22,8 +22,10 @@ productos.forEach(function(producto){
 
     let tarjeta = document.createElement('div');
 
-    tarjeta.classList.add('card')
-    tarjeta.classList.add('h-100')
+    tarjeta.classList.add('card');
+    tarjeta.classList.add('h-100');
+    tarjeta.classList.add('tarjeta__producto');
+    
 
     let img = document.createElement('img');
     
@@ -31,14 +33,43 @@ productos.forEach(function(producto){
     img.classList.add('p-4');
     img.src = producto.img;
 
+    let cuerpo = document.createElement('div');
+
+    cuerpo.classList.add('card-body');
+
     let titulo = document.createElement('h4');
 
     titulo.classList.add('card-title');
     titulo.classList.add('text-center');
+    titulo.setAttribute('style', 'color: white; font-family: Fugaz One, cursive;');
     titulo.textContent = producto.nombre;
 
-    img.appendChild(tarjeta);
-    titulo.appendChild(tarjeta);
+    let descripcion = document.createElement('p');
+
+    descripcion.classList.add('card-text');
+    descripcion.setAttribute('style', 'color: white; text-align: center;');
+    descripcion.textContent = producto.desc;
+
+    let precio = document.createElement('p');
+
+    precio.classList.add('precio__tarjeta');
+    precio.setAttribute('style', 'color: white; text-align: center; font-weight: bolder; font-size: 2em;');
+    precio.textContent = '$' + producto.precio;
+
+
+    let boton = document.createElement('a');
+
+    boton.classList.add('btn');
+    boton.classList.add('boton__tarjeta');
+    boton.textContent = 'Ver Producto';
+
+    tarjeta.appendChild(img);
+    tarjeta.appendChild(cuerpo);
+
+    cuerpo.appendChild(titulo);
+    cuerpo.appendChild(descripcion);
+    cuerpo.appendChild(precio);
+    cuerpo.appendChild(boton);
 
     columna.appendChild(tarjeta);
     fila.appendChild(columna);
