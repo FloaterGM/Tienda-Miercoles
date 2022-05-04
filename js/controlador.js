d46b1cd962acc8eedc065abba4f4431e5d5d794e
FloaterGM <99337469+FloaterGM@users.noreply.github.com>
2022-05-04 05:14:41 -0500

    Se termina registro y se pulen detalles

--- js/controlador.js
+++ js/controlador.js
@@ -10,34 +10,39 @@ let fila_contenedora = document.getElementById('fila');
 let informacion = new bootstrap.Modal(document.getElementById('modal_info'));
 let resumen_compra = new bootstrap.Modal(document.getElementById('resumen_compra'));
 
-fila_contenedora.addEventListener('click', function(evento){
-    if(evento.target.classList.contains('btn')){
+fila_contenedora.addEventListener('click', function (evento) {
+    if (evento.target.classList.contains('btn')) {
 
         producto = ampliar_info(evento);
-        
+
         console.log(producto);
         informacion.show();
     }
-}) 
-
+})
 
 let productos = [];
+
+
+let cerrar = document.getElementById('cerrar');
+cerrar.addEventListener('click', function () {
+    document.getElementById('cantidad_producto').value = '1';
+})
+
 let boton_carrito = document.getElementById('boton_agg');
-
-
-boton_carrito.addEventListener('click', function(){
-    let cantidad=document.getElementById('cantidad_producto').value
-    producto.cantidad=cantidad
+boton_carrito.addEventListener('click', function () {
+    let cantidad = document.getElementById('cantidad_producto').value;
+    producto.cantidad = cantidad;
 
     productos.push(producto);
 
-    let suma=0
-    productos.forEach(function(producto){
-        suma=suma+Number(producto.cantidad);
+    let suma = 0
+
+    productos.forEach(function (producto) {
+        suma = suma + Number(producto.cantidad);
     });
-    
-    let capsula=document.getElementById('capsula'); 
-    capsula.textContent=suma
+
+    let capsula = document.getElementById('capsula');
+    capsula.textContent = suma
     capsula.classList.remove('invisible');
 
     informacion.hide();
@@ -45,87 +50,81 @@ boton_carrito.addEventListener('click', function(){
     document.getElementById('cantidad_producto').value = '1';
 })
 
-let boton_v_carrito=document.getElementById('ver_carrito');
-boton_v_carrito.addEventListener('click',function(){
+let boton_v_carrito = document.getElementById('ver_carrito');
+boton_v_carrito.addEventListener('click', function () {
 
-    //recorrer el carrito y pintar los productos
-    let base=document.getElementById('base_carro')
+    let base = document.getElementById('base_carro')
 
-    base.innerHTML=''
+    base.innerHTML = ''
 
-    productos.forEach(function(producto){
+    productos.forEach(function (producto) {
 
-        let fila=document.createElement('div');
+        let fila = document.createElement('div');
         fila.classList.add('row');
 
-        let columna1=document.createElement('div');
+        let columna1 = document.createElement('div');
         columna1.classList.add('col-4');
 
-        let columna2=document.createElement('div');
+        let columna2 = document.createElement('div');
         columna2.classList.add('col-8');
 
-        let foto=document.createElement('img');
-        foto.classList.add('w-100','img-fluid');
-        foto.src=producto.foto;
+        let foto = document.createElement('img');
+        foto.classList.add('w-100', 'img-fluid');
+        foto.src = producto.foto;
 
-        let nombre=document.createElement('h1');
+        let nombre = document.createElement('h1');
         nombre.classList.add('text-align-center');
-        nombre.textContent=producto.nombre;
+        nombre.textContent = producto.nombre;
 
-        let cantidad=document.createElement('p');
+        let cantidad = document.createElement('p');
         cantidad.classList.add('text-align-center');
-        cantidad.textContent="Cantidad: " + producto.cantidad;
-        
-        let precio=document.createElement('p');
-        precio.classList.add('precio');
-        precio.textContent="Subtotal: " + (producto.precio * parseInt(producto.cantidad));
+        cantidad.textContent = "Cantidad: " + producto.cantidad;
 
+        let sub_t = document.createElement('p');
+        sub_t.classList.add('precio');
+        sub_t.textContent = "Subtotal: " + (producto.precio * parseInt(producto.cantidad));
 
+        producto.subtotal = (producto.precio * parseInt(producto.cantidad));
 
-        //PADRES E HIJOS
         columna2.appendChild(nombre);
         columna2.appendChild(cantidad);
-        columna2.appendChild(precio);
+        columna2.appendChild(sub_t);
         columna1.appendChild(foto);
         fila.appendChild(columna1);
         fila.appendChild(columna2);
         base.appendChild(fila);
 
     })
+    let total = document.getElementById('total_modal');
 
-    resumen_compra.show();
-})
-
-let modal_contenedor = document.getElementById('ver_carrito');
-let total = document.getElementById('total_modal');
-
-modal_contenedor.addEventListener('click', function(){
-
-    let suma_total = productos.map(producto => producto.precio).reduce((prev, curr) => parseInt(prev) + parseInt(curr));
-
-    
+    let suma_total = productos.map(producto => producto.subtotal).reduce((prev, curr) => parseInt(prev) + parseInt(curr));
 
     total.textContent = suma_total;
 
     document.getElementById('conversor').value = 0;
+
+   
+    
+
+    resumen_compra.show();
 })
+
+let total = document.getElementById('total_modal');
 
 let convertir = document.getElementById('conversor');
-convertir.addEventListener('click',function(){
-    let dolares, pesos;
-    if(document.getElementById('conversor').value == 0){
-            dolares = parseInt(total.textContent) / 3959.67;
-            let dol = dolares.toFixed(2)
-            document.getElementById('conversor').value = 1;
-
-            total.textContent = dol;
+convertir.addEventListener('click', function () {
+    if (document.getElementById('conversor').value == 0) {
+        total.textContent = parseFloat(total.textContent) / 4000;
+        document.getElementById('conversor').value = 1;
     }
-    else{
-            pesos = parseInt(total.textContent) * 3959.67;
-            let pes = pesos.toFixed(2)
-            document.getElementById('conversor').value = 0;
-
-            total.textContent = pes;
+    else {
+        total.textContent = parseFloat(total.textContent) * 4000;
+        document.getElementById('conversor').value = 0;
     }
 })
+
+
+
+
+
 
